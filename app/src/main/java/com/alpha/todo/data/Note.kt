4fee481fb5ce614d02fbd215a2a0880faa03d010
AlpherDev <alphaomondi001@gmail.com>
2022-11-15 10:39:26 +0300

    feat: Made our data parcelable.
    Data can now be passed from one fragment to the other

--- app/src/main/java/com/alpha/todo/data/Note.kt
+++ app/src/main/java/com/alpha/todo/data/Note.kt
@@ -1,17 +1,20 @@
 package com.alpha.todo.data
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 import java.text.DateFormat
 
 @Entity(tableName = "task_table")
+@Parcelize
 data class Note(
     val taskName: String,
-    val isImportant: Boolean
+    val isImportant: Boolean,
     val isCompleted: Boolean,
     val dateCreated: Long,
     @PrimaryKey(autoGenerate = true) val taskId: Int
-) {
+):Parcelable{
     val formattedDateCreated
         get() = DateFormat.getDateTimeInstance().format(dateCreated)
 }
