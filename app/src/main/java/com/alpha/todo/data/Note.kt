1bc2ac79170a1c75a8078444ca8d5d31a12e95a7
AlpherDev <alphaomondi001@gmail.com>
2022-11-15 10:43:50 +0300

    docs: Added comments in the data class

--- app/src/main/java/com/alpha/todo/data/Note.kt
+++ app/src/main/java/com/alpha/todo/data/Note.kt
@@ -6,15 +6,18 @@ import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.text.DateFormat
 
+// annotation creates a table.
 @Entity(tableName = "task_table")
+// this annotation enables data to be passed from one fragment to another.
 @Parcelize
 data class Note(
     val taskName: String,
     val isImportant: Boolean,
     val isCompleted: Boolean,
     val dateCreated: Long,
+    //annotation enables each task to be unique
     @PrimaryKey(autoGenerate = true) val taskId: Int
-):Parcelable{
+) : Parcelable {
     val formattedDateCreated
         get() = DateFormat.getDateTimeInstance().format(dateCreated)
 }
